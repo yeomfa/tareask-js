3f842029a9dd89c47537139a8d0d8c55086f30db
Omar Fandiño <omarfandinoya@gmail.com>
2023-04-03 18:18:02 -0500

    Improvements to the application: responsive design, localStorage, visual enhancements, and bug fixes

--- main.js
+++ main.js
@@ -14,10 +14,20 @@ const toggleTheme = document.querySelector('.theme-toggle');
 const btnGithub = document.querySelector('a');
 
 // Variables
-let tasks = [{content: 'Terminar de aprender JavaScript 🤖', success: false}, {content: 'Terminar de aprender rust 🦀', success: false}, {content: 'Avanzar en DevOps 🌀', success: false}];
-let isDark = false;
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [{content: 'Terminar de aprender JavaScript 🤖', success: false}, {content: 'Terminar de aprender rust 🦀', success: false}, {content: 'Avanzar en DevOps 🌀', success: false}];
+let isDark = JSON.parse(localStorage.getItem('theme'));
+console.log(isDark);
+const emojis = ["🐙", "📱", "🐺", "🦋", "🕷", "🐢", "🐴", "🦄", "🤖", "🐵", "🦉", "🐛", "🦞", "🦀", "🐮", "🐧", "🐸", "🦕", "🐯", "🐠", "🐍", "🦑", "🐝", "🦈", "🐳", "🦐", "🦂", "🌀", "🐭", "🐞", "🐻", "🐊", "🐷", "🦅", "🦎", "🐋", "🐙", "🐘", "🦇", "🦟", "🐠", "🐬", "🐢", "🐺", "🐨", "📷", "🐡", "🐦", "🎧", "🦔", "🐌", "🎓", "🚀", "🐜", "🦃", "🐔", "🦜", "🦔", "🏫", "🐼", "🕸️", "🦌", "🐑", "🐣", "📚", "🦕", "🕹️", "🦈"];
+
+// console.log(tasks);
+// localStorage.setItem('tasks', JSON.stringify(tasks));
+// console.log(JSON.parse(localStorage.getItem('tasks')));
 
 // Functions
+const saveTasks = tasks => {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 const changeTheme = isDark => {
   const [theme, icon] = isDark ? ['dark', 'sun'] : ['light', 'moon'];
 
@@ -34,7 +44,6 @@ const changeTheme = isDark => {
   toggleTheme.innerHTML = `<i class='bx bxs-${icon}'></i>`;
   toggleTheme.style.color = `var(--${theme}-focus)`;
 }
-changeTheme(isDark);
 
 const insertEvents = () => {
   const btnsSuccess = [...allBtnsSuccess];
@@ -55,24 +64,35 @@ const insertEvents = () => {
 
 const chargeTasks = tasks => {
   todoList.innerHTML = '';
-  tasks.forEach(task => {
+  if (tasks.length !== 0) {
+    tasks.forEach(task => {
+      const template = document.createElement('div');
+      template.classList.add('todo');
+      template.innerHTML = 
+        ` <p class="todo-content ${task.success ? 'success' : ''}">${task.content}</p>
+          <div class="options">
+            <button class="btn-success"><i class='bx bx-check' ></i></button>
+            <button class="btn-delete"><i class='bx bx-x' ></i></button>
+          </div>`;
+      todoList.prepend(template);
+    });
+    insertEvents();
+  } else {
     const template = document.createElement('div');
-    template.classList.add('todo');
-    template.innerHTML = 
-      ` <p class="todo-content ${task.success ? 'success' : ''}">${task.content}</p>
-        <div class="options">
-          <button class="btn-success"><i class='bx bx-check' ></i></button>
-          <button class="btn-delete"><i class='bx bx-x' ></i></button>
-        </div>`;
+    template.classList.add('message');
+    template.textContent = 'Add a todo!';
+
     todoList.prepend(template);
-  });
-  insertEvents();
+  }
+  saveTasks(tasks);
+  changeTheme(isDark);
 }
 chargeTasks(tasks);
 
 const addTask = taskContent => {
   if (taskContent.length > 1) {
-    tasks.push({content: taskContent, success: false});
+    const random = Math.floor(Math.random() * emojis.length);
+    tasks.push({content: `${taskContent} ${emojis[random]}`, success: false});
     chargeTasks(tasks);
     inputTask.value = '';
   }
@@ -83,13 +103,16 @@ const addTask = taskContent => {
 const removeTask = taskPos => {
   const taskToRemove = tasks[taskPos].content;
   tasks = tasks.filter(task => task.content != taskToRemove); 
+  saveTasks(tasks);
   chargeTasks(tasks);
+  changeTheme(isDark);
 }
 
 const successTask = (taskPosCollection, taskPosArr) => {
   tasks[taskPosArr].success = true;
   const taskToSuccess = [...allTodosContent][taskPosCollection];
   taskToSuccess.classList.add('success');
+  saveTasks(tasks);
 }
 
 const checkHeight = () => {
@@ -108,4 +131,5 @@ btnAddTask.addEventListener('click', e => {
 toggleTheme.addEventListener('click', () => {
   isDark = !isDark;
   changeTheme(isDark);
+  localStorage.setItem('theme', isDark);
 });
